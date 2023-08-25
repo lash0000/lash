8d70e03ad90afe0c04cf3c7cff097a1e86861e0f
eyelash128 <kenbusano2003@gmail.com>
2023-08-25 19:02:42 +0800

    color change workin

--- _poly.js
+++ _poly.js
@@ -7,3 +7,30 @@
  */
 
 //toggle for others now
+
+const colorToggle = document.getElementById("colorToggle");
+var body = document.body;
+
+const colorPref = (value) => {
+  localStorage.setItem("colorPref", value);
+};
+
+function toggleDarkMode() {
+  body.classList.toggle("dark-mode");
+
+  // Toggle the icon
+  if (body.classList.contains("dark-mode")) {
+    colorPref("on");
+  } else {
+    colorPref("off");
+  }
+}
+
+const localPref = localStorage.getItem("colorPref");
+if (localPref === "on") {
+  body.classList.add("dark-mode");
+}
+
+colorToggle.addEventListener("click", toggleDarkMode);
+
+//toggle on scroll next
