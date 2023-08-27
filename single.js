ff47f9a341d1b911a6520f8f2fb1a9410549a379
lash0000 <kenbusano2003@gmail.com>
2023-08-27 10:57:49 +0800

    sdfdfsd

---
+++ single.js
@@ -0,0 +1,12 @@
+async function singleData() {
+  var singleData = await Promise.all([
+    fetch("data/articles/headline.xml").then((response) => response.text()),
+  ]);
+
+  var parser = new DOMParser();
+  var xmlDoc1 = parser.parseFromString(data1, "text/xml");
+  var xmlDoc2 = parser.parseFromString(data2, "text/xml");
+
+  return [xmlDoc1, xmlDoc2];
+}
+
